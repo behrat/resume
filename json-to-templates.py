84bfeb8ca0ea8374393aaef571101d7555b5a935
Braden Ehrat <braden@bradenehrat.com>
2015-09-08 11:32:18 -0500

    Added 2014 files from svn

---
+++ json-to-templates.py
@@ -0,0 +1,17 @@
+import json
+import jinja2
+
+resume_stream = file('resume.json', 'r')
+resume = json.load(resume_stream)
+
+jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader('templates'),
+        trim_blocks=True,    
+        lstrip_blocks=True,
+)
+
+txt_template = jinja_env.get_template('template.txt')
+resume_txt = txt_template.render(resume=resume)
+
+with open("output/resume-ehrat.txt", "wb") as fh:
+    fh.write(resume_txt.encode('utf-8'))
