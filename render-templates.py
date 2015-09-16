81d7b17c770d248eb0bf2eaaf5623ea699dadcc3
Braden Ehrat <braden@bradenehrat.com>
2015-09-16 12:43:52 -0500

    Working MediaWiki template

--- render-templates.py
+++ render-templates.py
@@ -10,38 +10,6 @@ with open('configs/default.yaml', 'r') as f:
     resume_config = f.read()
 
 resume = yaml.load(resume_data + resume_config)
-#resume_stream = file('resume.yaml', 'r')
-#resume = yaml.load(resume_stream)
-
-JINJA_TEMPLATES_FOLDER = 'templates'
-
-jinja_env = jinja2.Environment(
-        loader = jinja2.FileSystemLoader(JINJA_TEMPLATES_FOLDER),
-        trim_blocks = True,    
-        lstrip_blocks = True,
-)
-
-latex_jinja_env = jinja2.Environment(
-        loader = jinja2.FileSystemLoader(JINJA_TEMPLATES_FOLDER),
-        block_start_string = '\BLOCK{',
-        block_end_string = '}',
-        variable_start_string = '\VAR{',
-        variable_end_string = '}',
-        comment_start_string = '\#{',
-        comment_end_string = '}',
-#        line_statement_prefix = '%-',
-        line_statement_prefix = None,
-        line_comment_prefix = '%#',
-        trim_blocks = True,
-        lstrip_blocks = True,
-        autoescape = False,
-)
-
-latex_template = latex_jinja_env.get_template('template.tex')
-resume_tex = latex_template.render(resume=resume)
-
-txt_template = jinja_env.get_template('template.txt')
-resume_txt = txt_template.render(resume=resume)
 
 try:
     os.makedirs("output")
@@ -49,8 +17,53 @@ except OSError as exception:
     if exception.errno != errno.EEXIST:
         raise
 
+JINJA_TEMPLATES_FOLDER = 'templates'
+jinja_loader = jinja2.FileSystemLoader(JINJA_TEMPLATES_FOLDER)
+
+txt_jinja_env = jinja2.Environment(
+        loader = jinja_loader,
+        trim_blocks = True,    
+        lstrip_blocks = True,
+)
+
+txt_template = txt_jinja_env.get_template('template.txt')
+resume_txt = txt_template.render(resume=resume)
+
 with open("output/resume-ehrat.txt", "wb") as fh:
     fh.write(resume_txt.encode('utf-8'))
 
+latex_jinja_env = jinja2.Environment(
+        loader = jinja_loader,
+        block_start_string = '\BLOCK{',
+        block_end_string = '}',
+        variable_start_string = '\VAR{',
+        variable_end_string = '}',
+        comment_start_string = '\#{',
+        comment_end_string = '}',
+        trim_blocks = True,
+        lstrip_blocks = True,
+)
+
+latex_template = latex_jinja_env.get_template('template.tex')
+resume_tex = latex_template.render(resume=resume)
+
 with open("output/resume-ehrat.tex", "wb") as fh:
     fh.write(resume_tex.encode('utf-8'))
+
+wiki_jinja_env = jinja2.Environment(
+        loader = jinja_loader,
+        block_start_string='<block>',
+        block_end_string='</block>',
+        variable_start_string='<var>',
+        variable_end_string='</var>',
+#        comment_start_string='<#',
+#        comment_end_string='<#',
+        trim_blocks = True,
+        lstrip_blocks = True,
+)
+
+wiki_template = wiki_jinja_env.get_template('template.wiki')
+wiki_resume = wiki_template.render(resume=resume)
+
+with open("output/resume-ehrat.wiki", "wb") as fh:
+    fh.write(wiki_resume.encode('utf-8'))
